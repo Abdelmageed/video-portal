1b8f29d29c4f9945665f6246c7f5d2704b331c43
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-04-07 22:10:54 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Crossover Video Portal backend
+This is the backend API code that needs to be consumed by front-end applications.
---
+++ config.js
@@ -0,0 +1,6 @@
+var configs = {};
+configs.applicationPort = 3000;
+configs.dbName = 'CrossoverVideosAssignment';
+configs.dbHost = 'localhost';
+
+module.exports = configs;
