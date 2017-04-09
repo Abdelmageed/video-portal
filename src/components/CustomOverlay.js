2edebc52563d7cd59a2e2b13c17150a15ead9cf1
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-04-09 15:30:40 +0200

    CustomOverlay component (migrated from boilerplate).

---
+++ src/components/CustomOverlay.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {Button, Overlay, NavItem} from 'react-bootstrap';
+
+export default class CustomOverlay extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      show: false  
+    };
+    
+    this.toggle = this.toggle.bind(this);
+  }
+  
+  toggle(){
+    this.setState({
+      show: !this.state.show
+    });
+  }
+  
+  render(){
+    return (
+      <NavItem style={{
+            position: "relative"
+        }}>
+        <Button ref="target" onClick={()=>this.toggle()}>
+          {this.props.triggerText}
+        </Button>
+        <Overlay
+         show={this.state.show}
+         onHide={()=> this.setState({show: false})}
+         placement="bottom"
+         container={this}
+         rootClose
+         target={()=> ReactDOM.findDOMNode(this.refs.target)}
+         >
+          {this.props.popover}
+        </Overlay>
+      </NavItem>
+    );
+  }
+}
