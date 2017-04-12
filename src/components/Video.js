edf8daf1576611fa7bedb111042b9bf09b13929b
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-04-12 20:36:30 +0200

    Styles.

--- src/components/Video.js
+++ src/components/Video.js
@@ -9,7 +9,6 @@ export default class Video extends Component {
     super(props);
     
     this.videoRef = null;
-    
     this.handleOnPlay = this.handleOnPlay.bind(this);
   }
   
@@ -18,32 +17,72 @@ export default class Video extends Component {
   }
   
   render() {
+    const containerStyle = {
+      boxShadow: '0 2p 5px rgba(0, 0, 0, 0.4)',
+      backgroundColor: 'white',
+      textAlign: 'center',
+      padding: '10px',
+    };
+    
+    const titleStyle = {
+      textAlign: 'center',
+      fontFamily: 'verdana helvetica sans-serif',
+      fontSize: 18,
+      whiteSpace: 'nowrap',
+      textOverflow: 'ellipsis',
+      overflow: 'hidden'
+    };
+    
+    const playerStyle = {
+      width: '100%',
+      height: (this.props.isMain)? 500 : 300
+    };
+    
+    const descStyle = {
+      whiteSpace: 'nowrap',
+      textOverflow: 'ellipsis',
+      overflow: 'hidden',
+      width: 100,
+      
+    }
+    
+    const classNames = ['video-container'];
+    if (!this.props.isMain) {
+      classNames.push('list-video');
+    }
+    
     return (
-      <div>
-        <div className="video-title">
+      <div className={classNames.join(' ')}>
+        <div className="video-title" style={titleStyle}>
         {
-          (this.props.withLink)?
+          (this.props.isMain)?
+            this.props.name
+            :
            <Link to={`/video/${this.props.id}`}>
             {this.props.name}
            </Link>
-            :
-            this.props.name
         }
         </div>
-        <div className="video-player">
-          <video 
+        <div className="video-player" >
+          <video style={playerStyle}
            controls 
            ref={(ref) => {this.videoRef = ref;}}
            onPlay={this.handleOnPlay}
            src={this.props.source} />
-            
         </div>
-        <p className="video-description">
-          {this.props.description}
-        </p>
+         {
+          (this.props.isMain)?
+          <p className="video-description">
+            {this.props.description}
+          </p>
+            :
+          null
+         }
+          
         <VideoRating 
           id={this.props.id}
           ratings={this.props.ratings}
+          showEdit={this.props.isMain}
         />
       </div>
     )
