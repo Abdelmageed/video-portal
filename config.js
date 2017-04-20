50f6cff256cbc6571175da7ffa5a5ecf779aaa5f
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-04-20 22:14:17 +0200

    Port env variable fixed.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var configs = {};
-configs.applicationPort = (process.PORT)? process.PORT : 3000;
+configs.applicationPort = (process.env.PORT)? process.env.PORT : 3000;
 configs.dbName = (process.env.DB_NAME)? process.env.DB_NAME : 'CrossoverVideosAssignment';
 configs.dbHost = (process.env.DB_HOST)? process.env.DB_HOST : 'localhost';
 
