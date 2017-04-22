e67498983b8d8920151fcbfdeb2f99a45458830c
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-04-22 14:51:36 +0200

    Hardcode the domain name for now.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var configs = {};
 configs.applicationPort = (process.env.PORT)? process.env.PORT : 3000;
 configs.dbName = (process.env.DB_NAME)? process.env.DB_NAME : 'CrossoverVideosAssignment';
 configs.dbHost = (process.env.DB_HOST)? process.env.DB_HOST : 'localhost';
-configs.domainName = (process.env.DOMAIN_NAME) ?
-      process.env.DOMAIN_NAME : `http://localhost:${configs.applicationPort}/`
+configs.domainName = (process.env.NODE_ENV === 'production') ?
+      'https://react-video-portal.herokuapp.com/' : `http://localhost:${configs.applicationPort}/`
 
 module.exports = configs;
